1b31266dd6df4b876fd2181d8ab2e444598fd91a
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-05-01 12:19:19 +0530

    configured directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An API to do Reverse Image Search',
+    author='aneesh-aparajit',
+    license='MIT',
+)
