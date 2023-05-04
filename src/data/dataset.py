9222a18f1232745b9e4e3404c3f57cab9c297e75
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-05-04 11:56:31 +0530

    updated dataset, config, references

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,22 +1,75 @@
+import os
+from typing import Dict
+
+import albumentations as A
+import numpy as np
 import pandas as pd
 import torch
+import yaml
+from albumentations.pytorch import ToTensorV2
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
+from transformers import AutoTokenizer
+
+with open('../config/config_v1.yaml') as f:
+    config = yaml.safe_load(f)
 
 
-class ReverseImageDataset(Dataset):
-    def __init__(self, df: pd.DataFrame) -> None:
+class ClipForImageSearchDataset(Dataset):
+    def __init__(self, df: pd.DataFrame, split: str = 'train') -> None:
         super().__init__()
-        self.df = df # [image_path, text]
+        self.df = df # [image, title]
+        self.split = split
+        self.tokenizer = AutoTokenizer.from_pretrained(config['LANGUAGE']['TOKENIZER_CHECKPOINT'])
+        if self.split == 'train':
+            self.transform = A.Compose([
+                A.Resize(height=config['VISION']['TRANSFORMS']['HEIGHT'], width=config['VISION']['TRANSFORMS']['WIDTH']),
+                ToTensorV2(),
+            ])
+        elif self.split == 'test':
+            self.transform = A.Compose([
+                A.Resize(height=config['VISION']['TRANSFORMS']['HEIGHT'], width=config['VISION']['TRANSFORMS']['WIDTH']),
+                ToTensorV2(),
+            ])
+        else:
+            raise NotImplemented(f"Split {self.split} has not been implemented as of yet, please try either of ['train', 'test']")
     
     def __len__(self):
         return len(self.df)
     
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
         row = self.df.iloc[idx, :]
-        text = row['text']
-        image_path = row['image_path']
+        text = row['title']
+        image_path = row['image']
 
-        return {
-            'text': text, 
-            'image': image_path
-        }
+        inputs = self.tokenizer(
+            text=text, return_tensors='pt', 
+            truncation=True, padding='max_length', 
+            max_length=config['LANGUAGE']['MAX_LENGTH'], return_attention_mask=True, 
+            return_token_type_ids=False
+        )
+
+        inputs = {k:v.squeeze() for k, v in inputs.items()}
+
+        if self.split == 'train':
+            image = np.array(Image.open(os.path.join(config['DATA_DIR'], 'train_images', image_path)).convert('RGB'))
+        else:
+            image = np.array(Image.open(os.path.join(config['DATA_DIR'], 'test_images', image_path)).convert('RGB'))
+
+        image = self.transform(image=image)['image']
+        inputs['image'] = image
+        return inputs
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(os.path.join(config['DATA_DIR'], 'train.csv'))
+    dataset = ClipForImageSearchDataset(df=df, split='train')
+    print(f'Length: {len(dataset)}')
+    batch = dataset[0]
+    __import__('pprint').pprint({k:v.shape for k,v in batch.items()})
+
+    dataloader = DataLoader(dataset=dataset, batch_size=config['HYPERPARAMETERS']['BATCH_SIZE'], shuffle=True)
+
+    batch = next(iter(dataloader))
+
+    __import__('pprint').pprint({k:v.shape for k,v in batch.items()})
